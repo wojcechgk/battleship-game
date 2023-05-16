c894e410e3143f83a70bd78bf4464f6036386eb8
Wojciech <wojciechgk@gmail.com>
2023-05-16 22:59:16 +0200

    refactor server logic

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,27 @@
 import express from 'express'
 import ws from 'ws'
 import dotenv from 'dotenv'
-import { Game, Player } from './services/gameLogic'
+// import { Game, Player } from './services/gameLogic'
 import { randomUUID } from 'crypto';
+
+const players: {
+  socket: ws.WebSocket,
+  playerId: string,
+  board?: number[][],
+} [] = [];
+const state: {
+  gameStarted: boolean,
+  winner: string | null,
+  turn: string | null,
+  hits: number[][],
+  mishits: number[][],
+} = {
+  gameStarted: false,
+  winner: null,
+  turn: null,
+  hits: [],
+  mishits: [],
+};
 
 dotenv.config();
 const app = express();
@@ -16,16 +35,19 @@ server.on('upgrade', (request, socket, head) => {
   });
 });
 
-let game: Game
-const players: Player[] = []
 
-// Connection and player handling
 wsServer.on('connection', socket => {
   handlePlayerConnection(socket)
   socket.on('message', message => {
-    handlePlayerAction(message)
+    handlePlayerMessage(socket, message)
   })
 })
+
+const broadcastState = () => { 
+  wsServer.clients.forEach(client => {
+    client.send(state.toString())
+  })
+}
 
 const handlePlayerConnection = (socket: ws.WebSocket) => {
   if(players.length > 2) {
@@ -33,26 +55,40 @@ const handlePlayerConnection = (socket: ws.WebSocket) => {
     return
   }
   const playerId = randomUUID()
-  const player = new Player(playerId)
-  players.push(player)
-  if(!game) {
-    game = new Game()
+  players.push({socket, playerId})
+  if(players.length === 2) {
+    state.gameStarted = true
+    state.turn = players[0].playerId
   }
-  socket.send(playerId)
-  // send game state
+  broadcastState()
 }
 
-const handlePlayerAction = (message: ws.RawData) => {
-  // Turn raw data message to object
-  // Get action type from object
-  // Invoke proper action
-  console.log(message)
+const handlePlayerMessage = (socket: ws.WebSocket, message: ws.RawData) => {
+  const actionType = EActionType.board // TODO get action type from message
+  const playerIndex = players.findIndex(p => p.socket === socket)
+  if(!playerIndex) { 
+    console.log('No player found! Executing action: ', actionType )
+    return
+  }
+  if(actionType === EActionType.board) {
+    setPlayerBoard(playerIndex, message)
+  }
+  else if(actionType === EActionType.hit) {
+    handlePlayerHit(playerIndex, message)
+  }
+  broadcastState()
 }
 
-// // game play
-// player1.board.addShip({ type: 'shipType1', fields: [[0, 1]] })
-// // ...
-// // check after every board update
-// if(player1.board.isBoardReady() && player2.board.isBoardReady()) {
-//   // start game
-// }
+const setPlayerBoard = (playerIndex: number, message: ws.RawData) => {
+  const board = [[1,2],[4,2]] // TODO get board from message
+  players[playerIndex].board = board
+}
+
+const handlePlayerHit = (playerIndex: number, message: ws.RawData) => {
+  // set player board
+}
+
+enum EActionType {
+  board = "BOARD",
+  hit = "HIT"  
+}
